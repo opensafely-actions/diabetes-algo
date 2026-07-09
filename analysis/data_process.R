30f2b10fdab6c21f952859e3d9db1e4d2339ba9f
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2026-07-09 14:01:35 +0200

    Add v0.0.14 CHANGELOG entry and bump version string for checkout v7

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -12,7 +12,7 @@
 # 10 Save output dataset (data_processed.rds)
 ################################################################################
 
-print("diabetes-algo version: v0.0.13")
+print("diabetes-algo version: v0.0.14")
 
 ################################################################################
 # Import libraries and functions
